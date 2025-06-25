d0ce8162e6e955c745b4fe31c138c9726d7c4440
Sanjit Ghosh <sanjitkumarghosh0@gmail.com>
2025-06-26 00:22:52 +0600

    learn postgres join

--- forignkey.sql
+++ forignkey.sql
@@ -1,29 +1,35 @@
-CREATE Table "user"(
+CREATE Table "user" (
     id SERIAL PRIMARY KEY,
     username VARCHAR(25) NOT NULL
 )
 
-
-CREATE Table post(
+CREATE Table post (
     id SERIAL PRIMARY KEY,
     title text NOT NULL,
-    user_id INTEGER REFERENCES "user"(id)
+    user_id INTEGER REFERENCES "user" (id)
 )
 
-
-INSERT INTO "user" (username) VALUES
-    ('akash'),
+INSERT INTO
+    "user" (username)
+VALUES ('akash'),
     ('batash'),
     ('sagor'),
     ('nodi')
 
-
-INSERT INTO post (title, user_id) VALUES
-    ('title 1', 2),
+INSERT INTO
+    post (title, user_id)
+VALUES ('title 1', 2),
     ('title 2', 1),
     ('title 3', 4),
     ('title 4', 4)
 
+DROP Table post;
+
+DROP Table "user";
 
 SELECT * FROM "user";
+
 SELECT * FROM post
+
+SELECT title, username FROM post
+    JOIN "user" ON post.user_id = "user".id;
